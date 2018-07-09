69588fa4c6602b1053e6c347c3e939ba4ad3b1d2
Rahul Vijay <vk.rahul318@gmail.com>
2018-07-09 13:07:13 +0530

    lifecycle methods created with new files

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,18 @@
 import 'package:flutter/material.dart';
 
+import './product_manager.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('Easy List'),
-      ),
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.all(10.0),
-            child: RaisedButton(
-              onPressed: () {},
-              child: Text('Add Product'),
-            ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Easy List'),
           ),
-          Card(
-            child: Column(
-              children: <Widget>[
-                Image.asset('assets/food.jpg'),
-                Text('Food Paradise')
-              ],
-            ),
-          )
-        ],
-      ),
-    ));
+          body: ProductManager('Food Appliance')),
+    );
   }
 }
